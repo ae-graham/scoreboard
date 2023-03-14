facd6be183a5f8b2e675c46e331518aeb6dfcd43
Alex Graham <ae.graham93@gmail.com>
2023-03-14 17:23:54 +1100

    Broke app components into different modules

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,8 @@
 import React from "react";
+import Header from "./Header";
+import Team from "./Team";
+import Counter from "./Counter";
 
-const Header = (props) => {
-  return (
-    <header>
-      <h1>{props.title}</h1>
-      <span className="stats">Players: {props.totalPlayers}</span>
-    </header>
-  );
-}
-
-const Player = (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-        {props.name}
-      </span>
-
-      <Counter />
-    </div>
-  );
-}
-
-const Counter = () => {
-  const [score, setScore] = React.useState(0);
-
-  const incrementScore = () => {
-    setScore(prevScore => prevScore + 1);
-  }
-
-  const decrementScore = () => {
-    setScore(prevScore => prevScore - 1);
-  }
-
-  return (
-    <div className="counter">
-      <button className="counter-action decrement" onClick={() => decrementScore()}> - </button>
-      <span className="counter-score">{score}</span>
-      <button className="counter-action increment" onClick={() => incrementScore()}> + </button>
-    </div>
-  );
-
-}
 
 const App = () => {
   const [players, setPlayers] = React.useState([
@@ -76,7 +37,7 @@ const App = () => {
 
       {/* Players list */}
       {players.map(player =>
-        <Player
+        <Team
           name={player.name}
           id={player.id}
           key={player.id.toString()}
